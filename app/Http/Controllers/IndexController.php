576f5dbb93324d8a4a2f9c8c5397a622c89efc94
wt-boss <megnemodonfack@gmail.com>
2023-08-17 09:47:31 +0200

    adding hero section for no authentified user

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index(){
+
+        if(Auth::check()){
+            return redirect()->to('chat');
+        }
         return inertia('Index/Index');
     }
 }
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,103 @@
-<template>
-    <div>
-        welcom to isChat test
-    </div>
-</template>
-
 <script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+import { Link } from "@inertiajs/vue3";
 
+const navigation = [
+    { name: "Product", href: "#" },
+    { name: "Features", href: "#" },
+    { name: "Marketplace", href: "#" },
+    { name: "Company", href: "#" },
+];
+
+const mobileMenuOpen = ref(false);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<template>
+    <div class="bg-white">
+        <div class="relative isolate px-6 lg:px-8">
+            <div
+                class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                aria-hidden="true"
+            >
+                <div
+                    class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    style="
+                        clip-path: polygon(
+                            74.1% 44.1%,
+                            100% 61.6%,
+                            97.5% 26.9%,
+                            85.5% 0.1%,
+                            80.7% 2%,
+                            72.5% 32.5%,
+                            60.2% 62.4%,
+                            52.4% 68.1%,
+                            47.5% 58.3%,
+                            45.2% 34.5%,
+                            27.5% 76.7%,
+                            0.1% 64.9%,
+                            17.9% 100%,
+                            27.6% 76.8%,
+                            76.1% 97.7%,
+                            74.1% 44.1%
+                        );
+                    "
+                />
+            </div>
+            <div class="mx-auto max-w-2xl py-8 sm:py-14">
+                <div class="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+                <div class="text-center">
+                    <h1
+                        class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+                    >
+                        Chat no have limit with Infinite Solution Chat.
+                    </h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-600">
+                        The future of Cameroonian communication is here: Our app is the next generation of messaging technology, designed specifically for the people of Cameroon
+                    </p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <Link
+                            href="/login"
+                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >Login</Link
+                        >
+                        <Link
+                            href="/register"
+                            class="text-sm font-semibold leading-6 text-gray-900"
+                            >Register <span aria-hidden="true">→</span></Link
+                        >
+                    </div>
+                </div>
+            </div>
+            <div
+                class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                aria-hidden="true"
+            >
+                <div
+                    class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                    style="
+                        clip-path: polygon(
+                            74.1% 44.1%,
+                            100% 61.6%,
+                            97.5% 26.9%,
+                            85.5% 0.1%,
+                            80.7% 2%,
+                            72.5% 32.5%,
+                            60.2% 62.4%,
+                            52.4% 68.1%,
+                            47.5% 58.3%,
+                            45.2% 34.5%,
+                            27.5% 76.7%,
+                            0.1% 64.9%,
+                            17.9% 100%,
+                            27.6% 76.8%,
+                            76.1% 97.7%,
+                            74.1% 44.1%
+                        );
+                    "
+                />
+            </div>
+        </div>
+    </div>
+</template>
